9765bd8470059d0f941ed4ef425f2d9921d5535f
liu_wp <liu_weipinglove@163.com>
2019-05-30 20:26:19 +0800

    项目初始化

---
+++ ec-system/ec-discovery/src/main/java/com/ping/ecdiscovery/EcDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.ping.ecdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcDiscoveryApplication.class, args);
+    }
+
+}
